ccbf174cfb6c2fbff3ea047c8a168e4700d81189
Romulo <romulo@7bi.me>
2017-08-31 15:47:37 -0300

    doing: first steps with persistence

--- character/views.py
+++ character/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from character.models import type_character
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+def get_all_types(request):
+    alltypes = type_character.objects.all()
+    races = []
+    race = {}
+    for tipo in alltypes:
+        race["id"] = tipo.id
+        race["race"] = tipo.nm_type_character
+        races.append(race)
+    return HttpResponse(json.dumps(races), content_type='application/json')
--- dbz/urls.py
+++ dbz/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$','sagas.views.index')
+    url(r'^$','sagas.views.index'),
+    url(r'^type_character/','character.views.get_all_types')
 )
