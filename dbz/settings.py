5764dada40ed01cadc326cdfd0ec7927d4be320c
romulo <romulogarofalo>
2018-02-26 11:10:44 -0300

    change database to sql ite

--- dbz/settings.py
+++ dbz/settings.py
@@ -59,25 +59,25 @@ WSGI_APPLICATION = 'dbz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dbz',
-        'USER': 'homestead',
-        'PASSWORD': 'secret',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
